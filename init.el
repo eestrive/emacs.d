8f468a7860f0d4ce695f87c724324553ac43cb4c
qinzz <1287715668@qq.com>
2019-04-27 23:13:46 +0800

    add pyim中文输入法

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
 ;;光标设置
 ;;(set-default 'cursor-type
 ;;	     'hbar)
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -19,5 +26,13 @@
 (windmove-default-keybindings)
 ;;窗口主题
 (load-theme 'wombat)
+;;(load-theme 'solarized t)
 ;; max size window
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+;;(require-package 'color-theme-sanityinc-solarized)
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(require 'pyim)
+(pyim-basedict-enable)   ; 拼音词库
+(setq default-input-method "pyim")
