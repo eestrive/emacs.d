107ace0e72116cfe0c09ec76d43efc1d0801d5e3
qinzz <1287715668@qq.com>
2021-04-15 07:55:56 -0700

    update

--- init.el
+++ init.el
@@ -34,4 +34,11 @@
 ;; auto-complete
 (ac-config-default)
 
-
+;; helm
+(require 'helm-config)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(global-set-key (kbd "C-s") #'helm-occur-from-isearch)
+(global-set-key (kbd "M-s") #'helm-occur-visible-buffers)
+(helm-mode 1)
