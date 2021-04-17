bda3e40ad54a3cf145a3ab20cb30762f74cd1e47
bestqin <1287715668@qq.com>
2021-04-17 16:11:54 +0800

    ubuntu update

--- init.el
+++ init.el
@@ -5,13 +5,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(package-selected-packages (quote (auto-complete yasnippet helm)))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro Medium" :foundry "outline" :slant normal :weight normal :height 181 :width normal)))))
+ ;; '(default ((t (:family "Source Code Pro Medium" :foundry "outline" :slant normal :weight normal :height 181 :width normal))))
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 181 :width normal))))
+ )
 
 ;; 按键异常声音忽略
 (setq ring-bell-function 'ignore)
